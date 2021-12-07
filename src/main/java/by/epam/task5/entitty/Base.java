028d7621d116ed96a0496ca2f41d9afd2a618a21
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-12-07 23:52:48 +0300

    Update Base.java

--- src/main/java/by/epam/task5/entitty/Base.java
+++ src/main/java/by/epam/task5/entitty/Base.java
@@ -4,8 +4,13 @@ import by.epam.task5.exception.CustomException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.Properties;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -13,22 +18,42 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Base {
-    static final Logger logger = LogManager.getLogger();
+    static Logger logger = LogManager.getLogger();
+    private static final String BASE_PROPERTIES_PATH = "base.properties";
     private static Base instance;
     private static ReentrantLock lock = new ReentrantLock();
     private static ReentrantLock lockTerminal = new ReentrantLock();
     private static AtomicBoolean isExist = new AtomicBoolean(false);
     private final Deque<Terminal> terminalDeque = new ArrayDeque<>();
-    private static final int NUMBER_OF_TERMINALS = 10;
-    private static final int MAX_WAREHOUSE_CAPACITY = 1000;
     private static AtomicInteger currentWarehouseSize = new AtomicInteger();
-    private Semaphore semaphore = new Semaphore(NUMBER_OF_TERMINALS, true);
+    private Semaphore semaphore;
+
+    private final int NUMBER_OF_TERMINALS;
+    private final int MAX_WAREHOUSE_CAPACITY;
+
+    private final int DEFAULT_NUMBER_TERMINALS = 6;
+    private final int DEFAULT_CAPACITY = 1000;
 
     private Base() {
+        URL resource = getClass().getClassLoader().getResource(BASE_PROPERTIES_PATH);
+        Properties properties = new Properties();
+        if (resource != null) {
+            File file = new File(resource.getFile());
+            try (FileReader fileReader = new FileReader(file)) {
+                properties.load(fileReader);
+            } catch (IOException e) {
+                logger.error("Base properties file is unavailable");
+            }
+        }
+        String numberOfTerminals = properties.getProperty("numberOfTerminals");
+        NUMBER_OF_TERMINALS = (numberOfTerminals != null) ? Integer.parseInt(numberOfTerminals) : DEFAULT_NUMBER_TERMINALS;
         for (int i = 0; i < NUMBER_OF_TERMINALS; i++) {
             terminalDeque.add(new Terminal());
         }
-        currentWarehouseSize.set(MAX_WAREHOUSE_CAPACITY / 3);
+        String maxWarehouseCapacity = properties.getProperty("warehouseCapacity");
+        MAX_WAREHOUSE_CAPACITY = (maxWarehouseCapacity != null) ? Integer.parseInt(maxWarehouseCapacity) : DEFAULT_CAPACITY;
+        currentWarehouseSize.set(MAX_WAREHOUSE_CAPACITY / NUMBER_OF_TERMINALS);
+        semaphore = new Semaphore(NUMBER_OF_TERMINALS, true);
     }
 
     public static Base getInstance() {
@@ -46,12 +71,11 @@ public class Base {
         return instance;
     }
 
-    public Terminal getTerminal(Truck truck) throws CustomException {
+    public Terminal getTerminal(Truck truck) {
         Terminal freeTerminal = null;
         try {
             semaphore.acquire();
             lockTerminal.lock();
-            //TimeUnit.MILLISECONDS.sleep(500);
             freeTerminal = terminalDeque.poll();
             logger.info("Truck " + truck.getId() + " got terminal " + freeTerminal.getTerminalId());
         } catch (InterruptedException e) {
